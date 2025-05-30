9358969768491a47c2b16d20d5a4eb43aec60dad
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-05-30 17:23:53 +0200

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+      <h1 className="text-4xl font-bold">Welcome to Rovix!</h1>
       <p className="mt-4 text-lg ">
-        This is a simple Next.js application using Geist fonts.
+        This is a simple Next.js application using Tailwind CSS for styling.
       </p>
     </main>
 
